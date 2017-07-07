58dd7cfaffc040062db0fc36ac1bd20eb85456f0
Dema Abu Adas <dabuadas@mail.uoguelph.ca>
2017-07-07 18:50:10 -0400

    Initial commit sorta

---
+++ dred/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  dred
+//
+//  Created by Dema. on 7/6/17.
+//  Copyright © 2017 Dema Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Connect the UI to the code
+    @IBOutlet weak var peebtn: UIButton!
+    @IBOutlet weak var drinkbtn: UIButton!
+    @IBOutlet weak var peecntr: UILabel!
+    @IBOutlet weak var drinkcntr: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
