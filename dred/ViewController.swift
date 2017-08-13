f9934073ce76c975fefdbeaa44beb8576340e11f
Dema Abu Adas <dabuadas@mail.uoguelph.ca>
2017-08-12 22:33:19 -0400

    added some functionality

--- dred/ViewController.swift
+++ dred/ViewController.swift
@@ -11,16 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     
     //Connect the UI to the code
+    @IBOutlet weak var drankPick: UIPickerView!
     @IBOutlet weak var peebtn: UIButton!
     @IBOutlet weak var drinkbtn: UIButton!
     @IBOutlet weak var peecntr: UILabel!
     @IBOutlet weak var drinkcntr: UILabel!
-
+    public var peeCount = 0;
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+//        self.drankPick.delegate = self
+//        self.drankPick.dataSource = self
+        pickerData = [ "250 mL", "500 mL", "600 mL", "750 mL", "1000 mL"]
+        
+        peebtn.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), for: .touchUpInside)
+        drinkbtn.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), for: .touchUpInside)
+        
+        
     }
 
+    func buttonClicked(_ sender: AnyObject?) {
+        if sender === peebtn {
+            // do something
+            peeCount += 1;
+            peecntr.text = String(peeCount) + " trips";
+        } else if sender === drinkbtn {
+            // do something
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
