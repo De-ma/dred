f604cd6307335dab32547e1bd4d9bcc635b44f10
Dema Abu Adas <dema@Demas-MacBook-Pro.local>
2017-10-06 23:34:50 -0400

    changed UI, made drinking functionable

--- dred/ViewController.swift
+++ dred/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var peecntr: UILabel!
     @IBOutlet weak var drinkcntr: UILabel!
     public var peeCount = 0;
+    public var drinkCount: Double = 0.0;
     
     var pickerData: [String] = [String]()
     
@@ -24,30 +25,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-//        self.drankPick.delegate = self
-//        self.drankPick.dataSource = self
-        pickerData = [ "250 mL", "500 mL", "600 mL", "750 mL", "1000 mL"]
-        
         peebtn.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), for: .touchUpInside)
         drinkbtn.addTarget(self, action: #selector(ViewController.buttonClicked(_:)), for: .touchUpInside)
         
         
     }
 
-    func buttonClicked(_ sender: AnyObject?) {
+    //Function to update if a button is clicked!
+    @objc func buttonClicked(_ sender: AnyObject?) {
         if sender === peebtn {
             // do something
             peeCount += 1;
             peecntr.text = String(peeCount) + " trips";
         } else if sender === drinkbtn {
             // do something
+            drinkCount += 0.6; //increments every 20 ounces
+            drinkcntr.text = String(drinkCount) + " L";
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
